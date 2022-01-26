c273c04eb27b99fb5b337699292103ddbccd7bec
Victor <huangv2@carleton.edu>
2022-01-26 15:12:13 -0600

    Finished program to find csv files and added a test csv file

--- most_active_cookie.py
+++ most_active_cookie.py
@@ -1,1 +1,39 @@
+import sys
+from datetime import date, datetime, time
 
+
+def usage():
+    print(f"Usage: {sys.argv[0]} <csv_file> -d \"yyyy-mm-dd\"")
+
+if len(sys.argv) != 4 or sys.argv[2] != '-d': #Check for valid user input
+    usage()
+    exit(1)
+
+c_tracker = {} #Creating a dictionary tracker to track cookie 
+
+c_hist = open('cookie_log.csv', "r") #Read and store the cookie history file into c_hist
+
+date_time = datetime.strptime(sys.argv[3], "%Y-%m-%d") #Convert date into a datetime object
+
+# Iterate through the data and split each csv line into readable format
+for line in c_hist.readlines(): 
+    c, t = line.split(",")
+    t = t.replace("\n", "")
+    # Convert the timestamp for each line into a readable datetime
+    tmp_datetime = datetime.strptime(t, '%Y-%m-%dT%H:%M:%S%z')
+    if date_time.strftime('%Y-%m-%d') == tmp_datetime.strftime('%Y-%m-%d'):
+        #f the cookie already exists, add one to its count
+        if c in c_tracker:
+            c_tracker[c] += 1
+        else:
+            # If the has not been seen before, create the cookie and set its count to 1
+            c_tracker[c] = 1
+
+
+max_occurred = max(c_tracker.values(), key= lambda x: x) #Return te max dictionary value (correspponds to the most active cookie)
+
+for cookie, occurrences in c_tracker.items(): #Print cookie(s) that is the most active (have the highest count)
+    if occurrences == max_occurred:
+        print(cookie)
+
+c_hist.close() #Close
